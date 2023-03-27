545dc0118782f1ea451db7920b3134ea5f1b0359
Anderson Furlan <anderson.furlan@thomsonreuters.com>
2023-03-27 20:15:34 -0300

    Create autor form page

--- src/app/autor/autor-routing.module.ts
+++ src/app/autor/autor-routing.module.ts
@@ -1,21 +1,30 @@
-import { NgModule } from "@angular/core";
-import { Route, RouterModule } from "@angular/router";
-import { AutorListPageComponent } from "./components/autor-list-page/autor-list-page.component";
+import { AutorFormPageComponent } from './components/autor-form-page/autor-form-page.component';
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { AutorListPageComponent } from './components/autor-list-page/autor-list-page.component';
 
 const routes: Route[] = [
-    {
-        path: '',
-        redirectTo: 'lista',
-        pathMatch: 'full'
-    },
-    {
-        path: 'lista',
-        component: AutorListPageComponent
-    }
-]
+  {
+    path: '',
+    redirectTo: 'lista',
+    pathMatch: 'full',
+  },
+  {
+    path: 'lista',
+    component: AutorListPageComponent,
+  },
+  {
+    path: 'cadastro',
+    component: AutorFormPageComponent,
+  },
+  {
+    path: 'edicao/:id',
+    component: AutorFormPageComponent,
+  },
+];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class AutorRoutingModule { }
+export class AutorRoutingModule {}
--- src/app/autor/autor.module.ts
+++ src/app/autor/autor.module.ts
@@ -1,14 +1,15 @@
-import { CommonModule } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { IonicModule } from "@ionic/angular";
-import { AutorRoutingModule } from "./autor-routing.module";
-import { AutorListPageComponent } from "./components/autor-list-page/autor-list-page.component";
-import { AutorService } from "./services/autor.service";
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { AutorRoutingModule } from './autor-routing.module';
+import { AutorFormPageComponent } from './components/autor-form-page/autor-form-page.component';
+import { AutorListPageComponent } from './components/autor-list-page/autor-list-page.component';
+import { AutorService } from './services/autor.service';
 
 @NgModule({
-    imports: [CommonModule, HttpClientModule, IonicModule, AutorRoutingModule],
-    declarations: [AutorListPageComponent],
-    providers: [AutorService]
+  imports: [CommonModule, HttpClientModule, IonicModule, AutorRoutingModule],
+  declarations: [AutorListPageComponent, AutorFormPageComponent],
+  providers: [AutorService],
 })
-export class AutorModule { }
+export class AutorModule {}
