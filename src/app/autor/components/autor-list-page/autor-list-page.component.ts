8d007f0d95c97af6572f7925fa025070d866a333
Anderson Furlan <anderson.furlan@thomsonreuters.com>
2023-03-27 21:49:19 -0300

    add remove author feature

--- src/app/autor/components/autor-list-page/autor-list-page.component.ts
+++ src/app/autor/components/autor-list-page/autor-list-page.component.ts
@@ -1,16 +1,50 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
-import { AutorService } from "../../services/autor.service";
-import { AutorInterface } from "../../types/autor.interface";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Observable, Subscription } from 'rxjs';
+import { AutorService } from '../../services/autor.service';
+import { AutorInterface } from '../../types/autor.interface';
 
 @Component({
-    selector: 'app-autor-list-page',
-    templateUrl: './autor-list-page.component.html'
-}) 
-export class AutorListPageComponent {
+  selector: 'app-autor-list-page',
+  templateUrl: './autor-list-page.component.html',
+})
+export class AutorListPageComponent implements OnInit, OnDestroy {
+  autores: AutorInterface[] = [];
+  subscriptions = new Subscription();
 
-    autores$: Observable<AutorInterface[]> = this.autorService.getAutores();
+  constructor(private autorService: AutorService, private alertController: AlertController) {}
 
-    constructor(private autorService: AutorService) {}
-   
-}  
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  listar() {
+    const subscription = this.autorService.getAutores().subscribe((autores) => {
+      this.autores = autores;
+    });
+    this.subscriptions.add(subscription);
+  }
+
+  async remove(autor: AutorInterface) {
+    const alert = await this.alertController.create({
+      header: 'Confirmação de exclusão',
+      message: `Deseja excluir o autor ${autor.nome}?`,
+      buttons: [
+        {
+          text: 'Sim',
+          handler: () => {
+            this.subscriptions.add(
+              this.autorService.remove(autor).subscribe(() => this.listar())
+            );
+          },
+        },
+        'Não',
+      ],
+    });
+    alert.present();
+  }
+}
--- src/app/autor/services/autor.service.ts
+++ src/app/autor/services/autor.service.ts
@@ -1,17 +1,22 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { environment } from "src/environments/environment";
-import { AutorInterface } from "../types/autor.interface";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AutorInterface } from '../types/autor.interface';
 
 @Injectable()
 export class AutorService {
+  constructor(private httpClient: HttpClient) {}
 
-    constructor(private httpClient: HttpClient) {}
+  getAutores(): Observable<AutorInterface[]> {
+    return this.httpClient.get<AutorInterface[]>(
+      `${environment.apiUrl}/autores`
+    );
+  }
 
-    getAutores(): Observable<AutorInterface[]> {
-        return this.httpClient.get<AutorInterface[]>(
-            `${environment.apiUrl}/autores`
-        )
-    }
+  remove(autor: AutorInterface): Observable<void> {
+    return this.httpClient.delete<void>(
+      `${environment.apiUrl}/autores/${autor.id}`
+    );
+  }
 }
