44d2d4b2dedcdfb4b6c9988ca267e7408368dec6
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-04-10 20:37:48 -0300

    ionic life cycle methods

--- src/app/autor/components/autor-form-page/autor-form-page.component.ts
+++ src/app/autor/components/autor-form-page/autor-form-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { AutorService } from '../../services/autor.service';
+import { Subscription } from 'rxjs';
+import { ViewDidEnter, ViewDidLeave, ViewWillEnter, ViewWillLeave } from '@ionic/angular';
 
 @Component({
   selector: 'app-autor-form-page',
   templateUrl: './autor-form-page.component.html',
 })
-export class AutorFormPageComponent implements OnInit {
+export class AutorFormPageComponent implements OnInit, OnDestroy,
+  ViewWillEnter, ViewDidEnter,
+  ViewWillLeave, ViewDidLeave {
 
   autorForm!: FormGroup
+
+  subscription = new Subscription()
 
   createMode: boolean = false;
   editMode: boolean = false;
@@ -20,6 +26,19 @@ export class AutorFormPageComponent implements OnInit {
     private formBuilder: FormBuilder,
     private autorService: AutorService,
   ) { }
+
+  ionViewWillEnter(): void {
+    console.log('ionViewWillEnter')
+  }
+  ionViewDidEnter(): void {
+    console.log('ionViewDidEnter')
+  }
+  ionViewWillLeave(): void {
+    console.log('ionViewWillLeave')
+  }
+  ionViewDidLeave(): void {
+    console.log('ionViewDidLeave')
+  }
 
   ngOnInit(): void {
     const [url] = this.activatedRoute.snapshot.url;
@@ -34,14 +53,22 @@ export class AutorFormPageComponent implements OnInit {
     })
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+
   save(): void {
     console.log(this.autorForm.value);
 
-    this.autorService.save(this.autorForm.value).subscribe(
-      () => {
-        // TODO mensagem de sucesso
-        this.router.navigate(['./autores'])
-      }
+    this.subscription.add(
+      this.autorService.save(this.autorForm.value).subscribe(
+        () => {
+          this.router.navigate(['./autores'])
+        },
+        () => {
+
+        }
+      )
     )
   }
 
